61cae8b0247fe8f789f11c4507203626ffc21014
liloxops <futbolero_07_14@hotmail.com>
2017-09-27 16:25:24 -0400

    Cree la clase Persona y un for en la main

--- Test_java/src/test_java/Test_java.java
+++ Test_java/src/test_java/Test_java.java
@@ -16,6 +16,10 @@ public class Test_java {
      */
     public static void main(String[] args) {
         System.out.println("Hola git!");
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println("asd");
+        }
     }
     
 }
